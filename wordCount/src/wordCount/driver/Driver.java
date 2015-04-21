d3a336ea4ad259b0786441a420da6b25a418cd00
Weronika Kowalczyk <wkowalc1@cs.binghamton.edu>
2015-04-21 14:27:57 -0400

    BST class

--- wordCount/src/wordCount/driver/Driver.java
+++ wordCount/src/wordCount/driver/Driver.java
@@ -1,7 +1,11 @@
 package wordCount.driver;
 
+import wordCount.treesForStrings.BST;
+
 public class Driver{
     public static void main(String[] args){
         System.out.println("I am a driver!");
+		
+
     }
 }
